7a90d351493820521010f05183912dbb731e7ac5
Danny <dannyberkelaar@gmail.com>
2019-11-06 10:53:44 +0100

    Bing maps init

---
+++ Festispec/POC-bingmaps/Employee.cs
@@ -0,0 +1,87 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml;
+
+namespace POC_bingmaps
+{
+    class Employee : INotifyPropertyChanged
+    {
+        private Map Map;
+
+        public string Adress { get; set; }
+
+        public string Name { get; }
+
+        public string Pos { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public ICommand SelectCommand { get; set; }
+
+        public Employee(string adress, string name, Map map)
+        {
+            Adress = adress;
+            Name = name;
+            Map = map;
+            SelectCommand = new RelayCommand(Select);
+            Geocode(adress);
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("SELECT POPPETJE " + Name);
+            Map.SelectedEmployee = this;
+        }
+
+        public void Geocode(string addressQuery)
+        {
+            //Create REST Services geocode request using Locations API
+            string geocodeRequest = "http://dev.virtualearth.net/REST/v1/Locations/" + addressQuery + "?o=xml&key=AttsGkqIHCOIEA11KtQZDphl5bi8lppin64jeg-ZOOhiS4cdHA_EXJwHSbyZi4Xo";
+
+
+            //Make the request and get the response
+            XmlDocument geocodeResponse = GetXmlResponse(geocodeRequest);
+
+            Latitude = float.Parse(geocodeResponse.GetElementsByTagName("Latitude").Cast<XmlNode>().First().InnerText);
+            Longitude = float.Parse(geocodeResponse.GetElementsByTagName("Longitude").Cast<XmlNode>().First().InnerText);
+            Pos = $"{Latitude},{Longitude}";
+            OnPropertyChanged("");
+        }
+
+        private XmlDocument GetXmlResponse(string requestUrl)
+        {
+            System.Diagnostics.Trace.WriteLine("Request URL (XML): " + requestUrl);
+            HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).",
+                    response.StatusCode,
+                    response.StatusDescription));
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(response.GetResponseStream());
+                return xmlDoc;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ Festispec/POC-bingmaps/Map.cs
@@ -0,0 +1,75 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace POC_bingmaps
+{
+    class Map
+    {
+        public ICommand AddCommand { get; set; }
+        public ICommand RemoveCommand { get; set; }
+
+        public string Name { get; set; }
+        public string Adress { get; set; }
+
+        private Employee _selectedEmployee;
+
+        public Employee SelectedEmployee
+        {
+            get
+            {
+                return _selectedEmployee;
+            }
+            set
+            {
+                _selectedEmployee = value;
+                OnPropertyChanged("");
+            }
+        }
+
+        public ObservableCollection<Employee> Employees { get; set; }
+
+        public Map()
+        {
+            Employees = new ObservableCollection<Employee>();
+            Employees.Add(new Employee("Den Bosch", "Geert", this) { Latitude = 13f, Longitude = 15f, Pos = "51.688641,5.887090" });
+            Employees.Add(new Employee("Den Bosch", "Peter", this) { Latitude = 15f, Longitude = 15f, Pos = "52.688641,5.287090" });
+
+            AddCommand = new RelayCommand(Add);
+            RemoveCommand = new RelayCommand(Remove);
+        }
+
+        private void Add()
+        {
+            var employee = new Employee(Adress, Name, this);
+            Employees.Add(employee);
+            Adress = "";
+            Name = "";
+            OnPropertyChanged("");
+        }
+
+        public void Remove()
+        {
+            Employees.Remove(SelectedEmployee);
+            SelectedEmployee = null;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
