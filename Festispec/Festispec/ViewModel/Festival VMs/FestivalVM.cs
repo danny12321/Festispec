49863a4cb43fb69237fec4d66153a1fa5a197c1e
Snowborn <jfa.vanderpoel@student.avans.nl>
2019-11-11 14:25:34 +0100

    begin festivalVM

---
+++ Festispec/Festispec/ViewModel/Festival VMs/FestivalVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace Festispec.ViewModel.Festival_VMs
+{
+    public class FestivalVM : ViewModelBase
+    {
+        /*private Festivals _festival;
+
+        public FestivalVM(Festivals festivals)
+        {
+            this._festival = festivals;
+        }
+
+
+        internal Festivals ToModel()
+        {
+            return _festival;
+        }*/
+    }
+}
