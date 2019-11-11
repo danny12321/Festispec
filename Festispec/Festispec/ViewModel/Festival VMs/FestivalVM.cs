cf2059b765f3de95222af13bc7853e5866253d14
Snowborn <jfa.vanderpoel@student.avans.nl>
2019-11-11 15:50:51 +0100

    connection string test

--- Festispec/Festispec/ViewModel/Festival VMs/FestivalVM.cs
+++ Festispec/Festispec/ViewModel/Festival VMs/FestivalVM.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Festispec.Domain;
 using GalaSoft.MvvmLight;
 
 namespace Festispec.ViewModel.Festival_VMs
 {
     public class FestivalVM : ViewModelBase
     {
-        /*private Festivals _festival;
+        private Festival _festival;
 
-        public FestivalVM(Festivals festivals)
+        public FestivalVM(Festival festivals)
         {
             this._festival = festivals;
         }
 
+        //properties van tabel toevoegen
 
-        internal Festivals ToModel()
+
+        internal Festival ToModel()
         {
             return _festival;
-        }*/
+        }
     }
 }
