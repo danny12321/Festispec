0e56053f7b72fa441f8807ef3147e357a82bc9a7
Snowborn <jfa.vanderpoel@student.avans.nl>
2019-11-13 18:51:00 +0100

    savecommand edit werkt

--- Festispec/Festispec/ViewModel/ClientVM/ClientInfoVM.cs
+++ Festispec/Festispec/ViewModel/ClientVM/ClientInfoVM.cs
@@ -1,15 +1,21 @@
-﻿using Festispec.ViewModel.DataService;
+﻿using Festispec.Domain;
+using Festispec.ViewModel.DataService;
+using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Festispec.ViewModel.ClientVM
 {
     public class ClientInfoVM
     {
         private IDataService _service;
+
+        public ICommand EditClientCommand { get; set; }
 
         public ClientsVM SelectedClient
         {
@@ -19,7 +25,21 @@ namespace Festispec.ViewModel.ClientVM
         public ClientInfoVM(IDataService service)
         {
             _service = service;
+            EditClientCommand = new RelayCommand(SaveClient, CanSaveClient);
         }
 
+        private bool CanSaveClient()
+        {
+            return true;
+        }
+
+        private void SaveClient()
+        {
+            using (var context = new FestispecEntities())
+            {
+                context.Entry(SelectedClient.ToModel()).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
     }
 }
