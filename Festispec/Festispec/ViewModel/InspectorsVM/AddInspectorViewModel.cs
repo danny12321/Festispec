c32e2ec435035aacb6921bd669d076b469f2bec2
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2019-11-14 10:34:41 +0100

    button disable en regex toegepast

--- Festispec/Festispec/ViewModel/InspectorsVM/AddInspectorViewModel.cs
+++ Festispec/Festispec/ViewModel/InspectorsVM/AddInspectorViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using Festispec.Domain;
-using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -22,13 +23,11 @@ namespace Festispec.ViewModel.InspectorsVM
             this._inspectors = Inspectors;
             this.Inspector = new InspectorviewModel();
 
-            AddInspectorCommand = new RelayCommand(AddInspectorMethod);
+            AddInspectorCommand = new RelayCommand(AddInspectorMethod, CanAddInspector);
         }
 
         private void AddInspectorMethod()
         {
-            if (CanAddInspector())
-            {
             Inspector.Active = DateTime.Now;
             _inspectors.Inspectors.Add(Inspector);
 
@@ -38,84 +37,83 @@ namespace Festispec.ViewModel.InspectorsVM
                 context.SaveChanges();
             }
             _inspectors.ShowInspectorPage();
-            }
+
         }
 
         public bool CanAddInspector()
         {
-            if (!IsEmptyField(Inspector.Housenumber) && !IsEmptyField(Inspector.InspectorFirstName) && !IsEmptyField(Inspector.InspectorLastName) && !IsEmptyField(Inspector.InspectorFirstName)
-                 && !IsEmptyField(Inspector.Phone) && !IsEmptyField(Inspector.PostalCode) && !IsEmptyField(Inspector.Street))
+            if(IsMatch())
             {
                 return true;
             }
             return false;
         }
 
-        //private bool IsMatch()
-        //{
-        //    if (IsLetter(Inspector.InspectorName) && IsLetterNumber(Inspector.PostalCode) && IsLetter(Inspector.Street) && IsNumber(Inspector.Housenumber) && IsLetter(Inspector.Country) && IsPhoneNumber(Inspector.Phone))
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
+        private bool IsMatch()
+        {
+            if (IsLetter(Inspector.InspectorFirstName) && IsLetter(Inspector.InspectorLastName) && IsLetterNumber(Inspector.PostalCode) && IsLetter(Inspector.Street) && IsNumber(Inspector.Housenumber) && IsPhoneNumber(Inspector.Phone))
+            {
+                return true;
+            }
+            return false;
+        }
 
-        //private bool IsLetter(string input)
-        //{
-        //    if (!IsEmptyField(input))
-        //    {
-        //        if (Regex.IsMatch(input, @"^^(?! )[A-Za-z\s]+$"))
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
+        private bool IsLetter(string input)
+        {
+            if (!IsEmptyField(input))
+            {
+                if (Regex.IsMatch(input, @"^^(?! )[A-Za-z\s]+$"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-        //private bool IsNumber(string input)
-        //{
-        //    if (!IsEmptyField(input))
-        //    {
-        //        if (Regex.IsMatch(input, @"^^(?! )[0-9\s]+$"))
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
+        private bool IsNumber(string input)
+        {
+            if (!IsEmptyField(input))
+            {
+                if (Regex.IsMatch(input, @"^^(?! )[0-9\s]+$"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-        //private bool IsLetterNumber(string input)
-        //{
-        //    if (!IsEmptyField(input))
-        //    {
-        //        if (Regex.IsMatch(input, @"^^(?! )[A-Za-z0-9\s]+$"))
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
+        private bool IsLetterNumber(string input)
+        {
+            if (!IsEmptyField(input))
+            {
+                if (Regex.IsMatch(input, @"^^(?! )[A-Za-z0-9\s]+$"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
         private bool IsEmptyField(string input)
         { 
            if (string.IsNullOrEmpty(input))
-         {
+           {
                return true;
-          }
+           }
            return false;
         }
 
-        //private bool IsPhoneNumber(string input)
-        //{
-        //    if (input == null)
-        //    {
-        //        return true;
-        //    }
-        //    else if (Regex.IsMatch(input, @"^^(?! )[0-9\s]+$"))
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
+        private bool IsPhoneNumber(string input)
+        {
+            if (input == null)
+            {
+                return true;
+            }
+            else if (Regex.IsMatch(input, @"^^(?! )[0-9\s]+$"))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
