eb738a9d2a3c7ac82085f91394c2607af4076b17
Nick <n.vanhoesel@hostingforyou.nl>
2019-11-19 22:47:57 +0100

    Inspecteur gelijk account toevoeging
    -Wachtwoord generatie
    -account word toegevoegd aan database

--- Festispec/Festispec/ViewModel/InspectorsVM/AddInspectorViewModel.cs
+++ Festispec/Festispec/ViewModel/InspectorsVM/AddInspectorViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Festispec.Domain;
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,30 +12,55 @@ using System.Windows.Input;
 
 namespace Festispec.ViewModel.InspectorsVM
 {
-    public class AddInspectorViewModel
+    public class AddInspectorViewModel : ViewModelBase
     {
         private InspectorListViewModel _inspectors;
 
         public InspectorviewModel Inspector { get; set; }
+        private String _password;
 
         public ICommand AddInspectorCommand { get; set; }
+        public ICommand CreatePass { get; set; }
 
+        public String Password
+        {
+            get
+            {
+                return _password;
+
+            }
+            set
+            {
+                _password = value;
+                base.RaisePropertyChanged("Password");
+            }
+        }
         public AddInspectorViewModel(InspectorListViewModel Inspectors)
         {
             this._inspectors = Inspectors;
             this.Inspector = new InspectorviewModel();
 
             AddInspectorCommand = new RelayCommand(AddInspectorMethod, CanAddInspector);
+            CreatePass = new RelayCommand(GeneratePassword);
         }
 
         private void AddInspectorMethod()
         {
             Inspector.Active = DateTime.Now;
             _inspectors.Inspectors.Add(Inspector);
-
+            var newuser = new Users();
+            newuser.email = Inspector.InspectorFirstName + Inspector.InspectorLastName.Replace(" ", string.Empty);
+            newuser.password = CalculateMD5Hash(Password);
+           
             using (var context = new FestispecEntities())
             {
                 context.Inspectors.Add(Inspector.ToModel());
+                //First time savechanges to generate inspector id wich we need before making a new user
+                context.SaveChanges();
+                var newinspector = context.Inspectors.Attach(Inspector.ToModel());
+                newuser.inspector_id = newinspector.id;
+                context.Users.Add(newuser);
+                
                 context.SaveChanges();
             }
             _inspectors.ShowInspectorPage();
@@ -66,6 +93,51 @@ namespace Festispec.ViewModel.InspectorsVM
            }
            return false;
         }
+        private string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+        private void GeneratePassword()
+        {
+            var passwd = CreatePassword(10);
+            Password = passwd;
+        }
+        public string CalculateMD5Hash(string input)
+
+        {
+
+            // step 1, calculate MD5 hash from input
+
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+
+            // step 2, convert byte array to hex string
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+
+            {
+
+                sb.Append(hash[i].ToString("X2"));
+
+            }
+
+            return sb.ToString();
+
+        }
+
 
     }
 }
