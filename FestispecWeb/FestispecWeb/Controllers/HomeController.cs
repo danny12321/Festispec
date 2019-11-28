9b3d9e6ae26e77d381fac793ff9731f6e7a23332
kiratahya <49717892+kiratahya@users.noreply.github.com>
2019-11-28 14:33:02 +0100

    WebApp

---
+++ FestispecWeb/FestispecWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FestispecWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Availability()
+        {
+            return View();
+        }
+
+        public ActionResult Planning()
+        {
+            return View();
+        }
+
+        public ActionResult Questionnaires()
+        {
+            return View();
+        }
+
+    }
+}
