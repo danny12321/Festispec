72dbe02e2ee166fee2e0be0cd793da6245b4da77
Snowborn <jfa.vanderpoel@student.avans.nl>
2019-12-03 15:42:01 +0100

    Vm toegevoegd

---
+++ FestispecWeb/FestispecWeb/Models/InspectionVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FestispecWeb.Models
+{
+    public class InspectionVM
+    {
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime Finished { get; set; }
+
+        public int FestivalId { get; set; }
+    }
+}
