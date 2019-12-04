14f91ab320d86aaf633405f6e971ad834603d9f4
Danny <d.berkelaar1@student.avans.nl>
2019-12-04 19:07:14 +0100

    Better schedule view

--- Festispec/Festispec/ViewModel/ScheduleVM/ScheduleVM.cs
+++ Festispec/Festispec/ViewModel/ScheduleVM/ScheduleVM.cs
@@ -16,24 +16,6 @@ namespace Festispec.ViewModel.ScheduleVM
     {
         public ObservableCollection<ScheduleColumnVM> Schedule { get; set; }
 
-        private int _columns = 4;
-
-        public int Columns
-        {
-            get { return _columns; }
-            set
-            {
-                if (value <= 31)
-                {
-                    _columns = value;
-                    Properties.Settings.Default.Schedule_Columns = value;
-                    Properties.Settings.Default.Save();
-                    RaisePropertyChanged("Columns");
-                    SetDates();
-                }
-            }
-        }
-
         private int _amountOfDaysToShow = 7;
 
         public int AmountOfDaysToShow
@@ -41,14 +23,11 @@ namespace Festispec.ViewModel.ScheduleVM
             get { return _amountOfDaysToShow; }
             set
             {
-                if (value <= 31)
-                {
-                    _amountOfDaysToShow = value;
-                    Properties.Settings.Default.Schedule_AmountOfDaysToShow = value;
-                    Properties.Settings.Default.Save();
-                    RaisePropertyChanged("AmountOfDaysToShow");
-                    SetDates();
-                }
+                _amountOfDaysToShow = value;
+                Properties.Settings.Default.Schedule_AmountOfDaysToShow = value;
+                Properties.Settings.Default.Save();
+                RaisePropertyChanged("AmountOfDaysToShow");
+                SetDates();
             }
         }
 
@@ -72,15 +51,20 @@ namespace Festispec.ViewModel.ScheduleVM
 
         public ICommand AddWeek { get; set; }
 
+        public ICommand SetWeekCommand { get; set; }
+
+        public ICommand SetMonthCommand { get; set; }
+
         public ScheduleVM()
         {
             Schedule = new ObservableCollection<ScheduleColumnVM>();
 
-            MinWeek = new RelayCommand(() => SelectedDate = SelectedDate.AddDays(-7));
-            AddWeek = new RelayCommand(() => SelectedDate = SelectedDate.AddDays(7));
-            AmountOfDaysToShow = Properties.Settings.Default.Schedule_AmountOfDaysToShow;
-            Columns = Properties.Settings.Default.Schedule_Columns;
+            MinWeek = new RelayCommand(() => SelectedDate = SelectedDate.AddDays(-AmountOfDaysToShow));
+            AddWeek = new RelayCommand(() => SelectedDate = SelectedDate.AddDays(AmountOfDaysToShow));
+            SetWeekCommand = new RelayCommand(() => AmountOfDaysToShow = 7);
+            SetMonthCommand = new RelayCommand(() => AmountOfDaysToShow = 31);
 
+            AmountOfDaysToShow = Properties.Settings.Default.Schedule_AmountOfDaysToShow;
 
             SetDates();
         }
