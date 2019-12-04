ba4aa68107d627e94ab3d67eb1b12c866b6a5dcc
kiratahya <49717892+kiratahya@users.noreply.github.com>
2019-12-04 12:52:47 +0100

    login

--- FestispecWeb/FestispecWeb/App_Start/RouteConfig.cs
+++ FestispecWeb/FestispecWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FestispecWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Homed", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
