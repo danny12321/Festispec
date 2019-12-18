3927dd4ebad54ccff1122e93653a9ac2f490dcfb
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2019-12-18 20:07:07 +0100

    Rollen systeem begin

---
+++ Festispec/Festispec/Utils/UserRole.cs
@@ -0,0 +1,30 @@
+﻿using Festispec.ViewModel.AccountsVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.Utils
+{
+    public class UserRole
+    {
+        public bool HasUserRole(UsersVM user, string[] roles)
+        {
+            var hasRole = false;
+
+            user.ToModel().Rolls.ToList().ForEach(u =>
+            {
+                for (int i = 0; i < roles.Length; i++)
+                {
+                    if (u.role == roles[i])
+                    {
+                        hasRole = true;
+                    }
+                }
+            });
+
+            return hasRole;
+        }
+    }
+}
